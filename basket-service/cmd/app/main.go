1285781221ea0acd2111d6bd484f117381b0c415
Alexander <federov2002@mail.ru>
2026-02-26 21:11:43 +0300

    add cmd main

--- basket-service/cmd/app/main.go
+++ basket-service/cmd/app/main.go
@@ -1,1 +1,256 @@
-package app
+package main
+
+import (
+	"basket-service/cmd"
+	httpin "basket-service/internal/adapters/in/http"
+	"basket-service/internal/adapters/out/postgres/basketrepo"
+	"basket-service/internal/adapters/out/postgres/goodrepo"
+	"basket-service/internal/core/domain/model/good"
+	"basket-service/internal/generated/servers"
+	"basket-service/internal/pkg/errs"
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	_ "github.com/lib/pq"
+	oam "github.com/oapi-codegen/echo-middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"net/http"
+	"os"
+)
+
+func main() {
+	configs := getConfigs()
+
+	connectionString, err := makeConnectionString(
+		configs.DbHost,
+		configs.DbPort,
+		configs.DbUser,
+		configs.DbPassword,
+		configs.DbName,
+		configs.DbSslMode)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	crateDbIfNotExists(configs.DbHost,
+		configs.DbPort,
+		configs.DbUser,
+		configs.DbPassword,
+		configs.DbName,
+		configs.DbSslMode)
+	gormDb := mustGormOpen(connectionString)
+	mustAutoMigrate(gormDb)
+
+	compositionRoot := cmd.NewCompositionRoot(
+		configs,
+		gormDb,
+	)
+	defer compositionRoot.CloseAll()
+
+	startWebServer(compositionRoot, configs.HttpPort)
+}
+
+func getConfigs() cmd.Config {
+	config := cmd.Config{
+		HttpPort:                  goDotEnvVariable("HTTP_PORT"),
+		DbHost:                    goDotEnvVariable("DB_HOST"),
+		DbPort:                    goDotEnvVariable("DB_PORT"),
+		DbUser:                    goDotEnvVariable("DB_USER"),
+		DbPassword:                goDotEnvVariable("DB_PASSWORD"),
+		DbName:                    goDotEnvVariable("DB_NAME"),
+		DbSslMode:                 goDotEnvVariable("DB_SSLMODE"),
+		DiscountServiceGrpcHost:   goDotEnvVariable("DISCOUNT_SERVICE_GRPC_HOST"),
+		KafkaHost:                 goDotEnvVariable("KAFKA_HOST"),
+		KafkaConsumerGroup:        goDotEnvVariable("KAFKA_CONSUMER_GROUP"),
+		KafkaStocksChangedTopic:   goDotEnvVariable("KAFKA_STOCKS_CHANGED_TOPIC"),
+		KafkaBasketConfirmedTopic: goDotEnvVariable("KAFKA_BASKET_CONFIRMED_TOPIC"),
+	}
+	return config
+}
+
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
+func crateDbIfNotExists(host string, port string, user string,
+	password string, dbName string, sslMode string) {
+	dsn, err := makeConnectionString(host, port, user, password, "postgres", sslMode)
+	if err != nil {
+		log.Fatalf("Ошибка подключения к PostgreSQL: %v", err)
+	}
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Ошибка подключения к PostgreSQL: %v", err)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("ошибка при закрытии db: %v", err)
+		}
+	}()
+
+	// Создаём базу данных, если её нет
+	_, err = db.Exec(fmt.Sprintf("CREATE DATABASE %s", dbName))
+	if err != nil {
+		log.Printf("Ошибка создания БД (возможно, уже существует): %v", err)
+	}
+}
+
+func makeConnectionString(host string, port string, user string,
+	password string, dbName string, sslMode string) (string, error) {
+	if host == "" {
+		return "", errs.NewValueIsRequiredError(host)
+	}
+	if port == "" {
+		return "", errs.NewValueIsRequiredError(port)
+	}
+	if user == "" {
+		return "", errs.NewValueIsRequiredError(user)
+	}
+	if password == "" {
+		return "", errs.NewValueIsRequiredError(password)
+	}
+	if dbName == "" {
+		return "", errs.NewValueIsRequiredError(dbName)
+	}
+	if sslMode == "" {
+		return "", errs.NewValueIsRequiredError(sslMode)
+	}
+	return fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=%v",
+		host,
+		port,
+		user,
+		password,
+		dbName,
+		sslMode), nil
+}
+
+func mustGormOpen(connectionString string) *gorm.DB {
+	pgGorm, err := gorm.Open(postgres.New(
+		postgres.Config{
+			DSN:                  connectionString,
+			PreferSimpleProtocol: true,
+		},
+	), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("connection to postgres through gorm\n: %s", err)
+	}
+	return pgGorm
+}
+
+func mustAutoMigrate(db *gorm.DB) {
+	err := db.AutoMigrate(&basketrepo.BasketDTO{})
+	if err != nil {
+		log.Fatalf("Ошибка миграции: %v", err)
+	}
+
+	err = db.AutoMigrate(&basketrepo.ItemDTO{})
+	if err != nil {
+		log.Fatalf("Ошибка миграции: %v", err)
+	}
+
+	err = db.AutoMigrate(&basketrepo.DeliveryPeriodDTO{})
+	if err != nil {
+		log.Fatalf("Ошибка миграции: %v", err)
+	}
+
+	err = db.AutoMigrate(&goodrepo.GoodDTO{})
+	if err != nil {
+		log.Fatalf("Ошибка миграции: %v", err)
+	}
+
+	// Seed
+	goodDTOs := make([]goodrepo.GoodDTO, len(good.Goods))
+	for i, goodAggregate := range good.Goods {
+		goodDTOs[i] = goodrepo.DomainToDTO(goodAggregate)
+	}
+
+	err = db.Clauses(clause.OnConflict{
+		UpdateAll: true, // обновит все поля, кроме PK
+	}).Create(&goodDTOs).Error
+	if err != nil {
+		log.Fatalf("Ошибка при вставке товаров: %v", err)
+	}
+}
+
+func startWebServer(compositionRoot *cmd.CompositionRoot, port string) {
+	handlers, err := httpin.NewServer(
+		compositionRoot.NewAddAddressCommandHandler(),
+		compositionRoot.NewAddDeliveryPeriodCommandHandler(),
+		compositionRoot.NewChangeItemsCommandHandler(),
+		compositionRoot.NewCheckoutCommandHandler(),
+		compositionRoot.NewGetBasketQueryHandler(),
+	)
+	if err != nil {
+		log.Fatalf("Ошибка инициализации HTTP Server: %v", err)
+	}
+
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
+	}))
+
+	spec, err := servers.GetSwagger()
+	if err != nil {
+		log.Fatalf("Error reading OpenAPI spec: %v", err)
+	}
+	e.Use(oam.OapiRequestValidator(spec))
+	e.Pre(middleware.RemoveTrailingSlash())
+	registerSwaggerOpenApi(e)
+	registerSwaggerUi(e)
+	servers.RegisterHandlers(e, handlers)
+	e.Logger.Fatal(e.Start(fmt.Sprintf("0.0.0.0:%s", port)))
+}
+
+func registerSwaggerOpenApi(e *echo.Echo) {
+	e.GET("/openapi.json", func(c echo.Context) error {
+		swagger, err := servers.GetSwagger()
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "failed to load swagger: "+err.Error())
+		}
+
+		data, err := swagger.MarshalJSON()
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "failed to marshal swagger: "+err.Error())
+		}
+
+		return c.Blob(http.StatusOK, "application/json", data)
+	})
+}
+
+func registerSwaggerUi(e *echo.Echo) {
+	e.GET("/docs", func(c echo.Context) error {
+		html := `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		  <meta charset="UTF-8">
+		  <title>Swagger UI</title>
+		  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css">
+		</head>
+		<body>
+		  <div id="swagger-ui"></div>
+		  <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+		  <script>
+			window.onload = () => {
+			  SwaggerUIBundle({
+				url: "/openapi.json",
+				dom_id: "#swagger-ui",
+			  });
+			};
+		  </script>
+		</body>
+		</html>`
+		return c.HTML(http.StatusOK, html)
+	})
+}
--- basket-service/go.mod
+++ basket-service/go.mod
@@ -5,8 +5,11 @@ go 1.25.5
 require (
 	github.com/getkin/kin-openapi v0.133.0
 	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.15.1
 	github.com/labstack/gommon v0.4.2
+	github.com/lib/pq v1.11.2
+	github.com/oapi-codegen/echo-middleware v1.0.2
 	github.com/oapi-codegen/runtime v1.1.2
 	github.com/stretchr/testify v1.11.1
 	github.com/testcontainers/testcontainers-go v0.40.0
@@ -38,6 +41,7 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
@@ -90,6 +94,7 @@ require (
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/time v0.14.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
